312381b8374fa7ae16ad0460336b7d765766d85f
Amin Wali <aminwali360@gmail.com>
2022-04-04 04:32:56 +0800

    update before school

---
+++ pageScript.js
@@ -0,0 +1,12 @@
+
+function changeToProgramming(){
+    window.location.href = "programming.html";
+}
+
+function changeToAbout(){
+    window.location.href = "about.html";
+}
+
+function changeToIndex(){
+    window.location.href = "index.html";
+}
