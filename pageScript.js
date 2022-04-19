106b4a41f151dfbc920f0a85d328c091440cce89
Amin Wali <aminwali360@gmail.com>
2022-04-20 02:10:14 +0800

    fixes iframne

--- pageScript.js
+++ pageScript.js
@@ -1,12 +1,12 @@
 
 function changeToProgramming(){
-    window.location.href = "programming.html";
+    top.window.location.href = "programming.html";
 }
 
 function changeToAbout(){
-    window.location.href = "about.html";
+    top.window.location.href = "about.html";
 }
 
 function changeToIndex(){
-    window.location.href = "index.html";
+    top.window.location.href = "index.html";
 }
