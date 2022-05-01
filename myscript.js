9840d4b6f0d177063b8a66cf8afa6f9e303f280d
Amin Wali <aminwali360@gmail.com>
2022-05-01 17:09:33 +0800

    image design without optimization

--- myscript.js
+++ myscript.js
@@ -43,8 +43,42 @@ function changeImageForSmallScreen(){
         console.log('hello');
     }
 }
+
+function changeRowForSmallScreen(){
+
+    if(window.innerWidth <600){
+        console.log("showing");
+        document.getElementById("mobileRow").style.display = 'block';
+        document.getElementById("mobileRow1").style.display = 'block';
+        document.getElementById("mobileRow2").style.display = 'block';
+        console.log('hello');
+        var x = document.getElementsByClassName("iconRow3");
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = 'none';
+        }
+    }
+    else{
+        console.log("hiding");
+        document.getElementById("mobileRow").style.display = 'none';
+        document.getElementById("mobileRow1").style.display = 'none';
+        document.getElementById("mobileRow2").style.display = 'none';
+        console.log('hello');
+
+        var x = document.getElementsByClassName("iconRow3");
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = 'block';
+        }
+    }
+
+}
+
+changeRowForSmallScreen();
+changeImageForSmallScreen();
  
 // Attaching the event listener function to window's resize event
 window.addEventListener("resize", changeImageForSmallScreen);
+window.addEventListener("resize", changeRowForSmallScreen);
 
 // Calling the function for the first time
