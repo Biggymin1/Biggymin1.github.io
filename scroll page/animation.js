53a2beb81acca9214daa7247c5b6b92f33f1c6d0
Amin Wali <aminwali360@gmail.com>
2023-03-06 00:44:16 +0800

    fixes issue where a single need to observe 2 object at same time causing one of them to be unobserved, added margin on mobile view.

--- scroll page/animation.js
+++ scroll page/animation.js
@@ -1,9 +1,24 @@
 
+//issue where 2 object need to be observe at same time, and only 1 is detected
+
 const observer = new IntersectionObserver((entries) => {
     console.log(entries);
 
     var e1 = entries[0];
-    //accessing value
+    
+    //check if itemRight
+    if(e1.target.id == "display-right"){
+        if(e1.isIntersecting == true){
+            console.log("item display opacity");
+            e1.target.classList.add("opacity-fade");
+        }
+        if(e1.isIntersecting == false){
+            console.log("item display hiding");
+            e1.target.classList.remove("opacity-fade");
+        }
+    }
+
+
     if(e1.isIntersecting == true){
         //intersection ratio ni bapak susahnya nak main
         e1.target.classList.replace("img-top-before","img-top");
@@ -29,15 +44,15 @@ observer.observe(hiddenElement);
 
 const itemDisplay = document.getElementsByClassName("item-display");
 
+const itemRight = document.querySelectorAll('[id=display-right]');
+
+console.log("right: "+ itemRight);
+
 newArray = Array.from(itemDisplay);
+rightArray = Array.from(itemRight);
+
 
 console.log("item display: " + itemDisplay);
-for(i in newArray){
-    console.log(newArray[i]); //notice ada 2 element extra
-}
-
-//cleaning that 2 element
-
 
 const observer2 = new IntersectionObserver((entries) => {
     console.log("entries");
@@ -59,8 +74,20 @@ const observer2 = new IntersectionObserver((entries) => {
         threshold: 0.1
       });      
 
+//separating into 2 observer, since some of them are observed at same point. left and center
+//observer 2, right observer 1 (just observer)
+
+
 for(i in newArray){
-    console.log("observing");
+    //console.log("observing");
     observer2.observe(newArray[i]);
-    console.log('success observed');
+    //console.log('success observed');
+}
+
+console.log("right: " + itemRight[0]);
+
+for(i in rightArray){
+    console.log("observing right");
+    observer.observe(rightArray[i]);
+    console.log('success observed right');
 }
